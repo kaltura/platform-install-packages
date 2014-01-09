8036a4b43f3ff441a46b05bb832b57d53ae3d5f1
Jess Portnoy <jess.portnoy@kaltura.com>
2014-01-09 04:28:44 -0500

    We don't need to source system.ini, if anything, it gets in the way. Also -echo when reading passwd.

--- RPM/scripts/postinst/kaltura-base-config.sh
+++ RPM/scripts/postinst/kaltura-base-config.sh
@@ -16,69 +16,73 @@
 #set -o nounset                              # Treat unset variables as an error
 verify_user_input()
 {
-	ANSFILE=$1
-	for VAL in TIME_ZONE KALTURA_FULL_VIRTUAL_HOST_NAME DB1_HOST DB1_PORT DB1_NAME DB1_USER DB1_PASS ; do
-		if [ -z "${!VAL}" ];then
-			echo "I need $VAL in $ANSFILE."
-			exit 1
-		fi
-	done
+        ANSFILE=$1
+        for VAL in TIME_ZONE KALTURA_FULL_VIRTUAL_HOST_NAME DB1_HOST DB1_PORT DB1_NAME DB1_USER DB1_PASS ; do
+                if [ -z "${!VAL}" ];then
+                        echo "I need $VAL in $ANSFILE."
+                        exit 1
+                fi
+        done
 }
-KALTURA_RC=/etc/kaltura.d/system.ini
-if [ ! -r $KALTURA_RC ];then
-	echo "Missing $KALTURA_RC."
-	exit 1
-fi
-. $KALTURA_RC
+#KALTURA_RC=/etc/kaltura.d/system.ini
+#if [ ! -r $KALTURA_RC ];then
+#       echo "Missing $KALTURA_RC."
+#       exit 1
+#fi
+#. $KALTURA_RC
 
 if [ -n "$1" -a -r "$1" ];then
-	ANSFILE=$1
-	verify_user_input $ANSFILE
+        ANSFILE=$1
+        verify_user_input $ANSFILE
 else
-	echo "Welcome to Kaltura Server $VERSION post install setup.
-	In order to finalize the system configuration, please input the following:
+        echo "Welcome to Kaltura Server `rpm -qa kaltura-base --queryformat %{version}` post install setup.
+In order to finalize the system configuration, please input the following:
 
-	CDN host [`hostname`]:
-	"
-	read -e CDN_HOST
-	if [ -z "$CDN_HOST" ];then
-		CDN_HOST=`hostname`
-	fi
+CDN host [`hostname`]:
+"
+        read -e CDN_HOST
+        if [ -z "$CDN_HOST" ];then
+                CDN_HOST=`hostname`
+        fi
 
-	echo "Apache virtual host: "
-	read -e KALTURA_VIRTUAL_HOST_NAME
-	if [ -z "$KALTURA_VIRTUAL_HOST_NAME" ];then
-		KALTURA_VIRTUAL_HOST_NAME=`hostname`
-	fi
-	echo "Which port will this Vhost listen on [443]? "
-	read -e KALTURA_VIRTUAL_HOST_PORT
-	if [ -z "$KALTURA_VIRTUAL_HOST_PORT" ];then
-		KALTURA_VIRTUAL_HOST_PORT=443
-	fi
-	KALTURA_FULL_VIRTUAL_HOST_NAME="$KALTURA_VIRTUAL_HOST_NAME:$KALTURA_VIRTUAL_HOST_PORT"
-	while [ -z "$DB1_HOST" ];do
-		echo "DB hostname: "
-		read -e DB1_HOST
-	done
+        echo "Apache virtual host: "
+        read -e KALTURA_VIRTUAL_HOST_NAME
+        if [ -z "$KALTURA_VIRTUAL_HOST_NAME" ];then
+                KALTURA_VIRTUAL_HOST_NAME=`hostname`
+        fi
+        echo "Which port will this Vhost listen on [443]? "
+        read -e KALTURA_VIRTUAL_HOST_PORT
+        if [ -z "$KALTURA_VIRTUAL_HOST_PORT" ];then
+                KALTURA_VIRTUAL_HOST_PORT=443
+        fi
+        KALTURA_FULL_VIRTUAL_HOST_NAME="$KALTURA_VIRTUAL_HOST_NAME:$KALTURA_VIRTUAL_HOST_PORT"
+        while [ -z "$DB1_HOST" ];do
+                echo "DB hostname: "
+                read -e DB1_HOST
+        done
 
-	echo "DB name [kaltura]: "
-	read -e DB1_NAME
-	if [ -z "$DB1_NAME" ];then
-		DB1_NAME=kaltura
-	fi
+        echo "DB name [kaltura]: "
+        read -e DB1_NAME
+        if [ -z "$DB1_NAME" ];then
+                DB1_NAME=kaltura
+        fi
 
-	while [ -z "$DB1_USER" ];do
-		echo "DB super user: "
-		read -e DB1_USER
-	done
-	while [ -z "$DB1_PASS" ];do
-		echo "DB super passwd: "
-		read -e DB1_PASS
-	done
-	while [ -z "$TIME_ZONE" ];do
-		echo "Your time zone [see http://php.net/date.timezone]: "
-		read -e TIME_ZONE
-	done
+        while [ -z "$DB1_USER" ];do
+                echo "DB super user: "
+                read -e DB1_USER
+        done
+        while [ -z "$DB1_PASS" ];do
+                echo "DB super passwd: "
+                STTY_ORIG=`stty -g`
+                stty -echo
+                read -e DB1_PASS
+                stty $STTY_ORIG
+
+        done
+        while [ -z "$TIME_ZONE" ];do
+                echo "Your time zone [see http://php.net/date.timezone]: "
+                read -e TIME_ZONE
+        done
 fi
 
 # Now we will sed.
