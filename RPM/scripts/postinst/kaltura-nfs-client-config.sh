b34a468a928eccd82ce479269bae098c451e0628
Jess Portnoy <jess.portnoy@kaltura.com>
2014-03-17 07:20:59 -0400

    create users and restart rpcbind.

--- RPM/scripts/postinst/kaltura-nfs-client-config.sh
+++ RPM/scripts/postinst/kaltura-nfs-client-config.sh
@@ -23,20 +23,32 @@ DOMAIN=$2
 NOBODY_USER=$3
 NOBODY_GROUP=$4
 IDMAPD_CONFFILE=/etc/idmapd.conf
-MOUNT_DIR=/opt/kaltura/web
+PREFIX=/opt/kaltura
+MOUNT_DIR=$PREFIX/web
+# create user/group, and update permissions
+groupadd -r kaltura -g7373 2>/dev/null || true
+useradd -M -r -u7373 -d $PREFIX -s /bin/bash -c "Kaltura server" -g kaltura kaltura 2>/dev/null || true
+getent group apache >/dev/null || groupadd -g 48 -r apache
+getent passwd apache >/dev/null || \
+  useradd -r -u 48 -g apache -s /sbin/nologin \
+    -d /var/www -c "Apache" apache
+usermod -g kaltura kaltura 2>/dev/null || true
+
 if grep -q "^Domain" $IDMAPD_CONFFILE;then
         sed -i "s@^Domain\s*=\s*.*@Domain = $DOMAIN@g" $IDMAPD_CONFFILE
 else
         echo "Domain = $DOMAIN" >> $IDMAPD_CONFFILE
 fi
 if grep -q "^Nobody-User" $IDMAPD_CONFFILE;then
-        sed -i "s@^Nobody-User\s*=\s*.*@Nobody-User = $NOBODY_USER@g" /etc/idmapd.conf
-        sed -i "s@^Nobody-Group\s*=\s*.*@Nobody-Group = $NOBODY_GROUP@g" /etc/idmapd.conf
+        sed -i "s@^Nobody-User\s*=\s*.*@Nobody-User = $NOBODY_USER@g" $IDMAPD_CONFFILE
+        sed -i "s@^Nobody-Group\s*=\s*.*@Nobody-Group = $NOBODY_GROUP@g" $IDMAPD_CONFFILE
 else
         echo "Nobody-User = $NOBODY_USER" >>$IDMAPD_CONFFILE
         echo "Nobody-User = $NOBODY_GROUP" >>$IDMAPD_CONFFILE
 fi
 echo "$NFS_HOST:$MOUNT_DIR $MOUNT_DIR nfs4" >> /etc/fstab
-service rpcidmapd restart
+for DAEMON in rpcbind rpcidmapd ;do
+	service $DAEMON restart
+done
 nfsidmap -c
 mount $MOUNT_DIR
