9a19d2669800b7c2217814f7de2a95b5c9b36f05
Jess Portnoy <jess.portnoy@kaltura.com>
2014-03-24 09:52:12 -0400

    more tests..

--- RPM/scripts/postinst/kaltura-sanity.sh
+++ RPM/scripts/postinst/kaltura-sanity.sh
@@ -43,6 +43,18 @@ for D in $ALL_DAEMONS; do
 			report "Check $D daemon status" 1 "Daemon $D is NOT running" "`bc <<< $END-$START`"
 		fi
 		START=`date +%s.%N`
+		if [ $D != 'kaltura-monit' ];then
+		if check_monit $KALTURA_VIRTUAL_HOST_NAME $D;then
+			END=`date +%s.%N`
+			TOTAL_T=`bc <<< $TIME`
+			report "Check $D daemon is started by Monit" 0 "Daemon $D is running" "`bc <<< $END-$START`"
+		else
+			END=`date +%s.%N`
+			TOTAL_T=`bc <<< $TIME`
+			report "Check $D daemon is started by Monit" 1 "Daemon $D is NOT running" "`bc <<< $END-$START`"
+		fi
+		fi
+		START=`date +%s.%N`
 		if check_daemon_init_status $KALTURA_VIRTUAL_HOST_NAME $D;then
 			END=`date +%s.%N`
 			TOTAL_T=`bc <<< $TIME`
@@ -71,65 +83,77 @@ report "check_testme_page" $RC "$MSG" "`bc <<< $END-$START`"
 
 START=`date +%s.%N`
 MSG=`check_kmc_index_page`
-END=`date +%s.%N`
 RC=$?
+END=`date +%s.%N`
 report "check_kmc_index_page" $RC "$MSG" "`bc <<< $END-$START`"
 
 START=`date +%s.%N`
 MSG=`check_admin_console_index_page`
-END=`date +%s.%N`
 RC=$?
+END=`date +%s.%N`
 report "check_admin_console_index_page" $RC "$MSG" "`bc <<< $END-$START`"
 
 DIRNAME=`dirname $0`
 ADMIN_PARTNER_SECRET=`echo "select admin_secret from partner where id=-2" | mysql -N -h $DB1_HOST -p$DB1_PASS $DB1_NAME -u$DB1_USER`
-START=`date +%s.%N`
 NOW=`date +%d-%H-%m-%S`
-END=`date +%s.%N`
+START=`date +%s.%N`
 PARTNER_ID=`php $DIRNAME/create_partner.php $ADMIN_PARTNER_SECRET mb-$NOW@kaltura.com testingpasswd $SERVICE_URL 2>&1`
 RC=$?
-TOTAL_T=`bc <<< $TIME`
+END=`date +%s.%N`
 report "Create Partner" $RC "New PID is $PARTNER_ID" "`bc <<< $END-$START`"
 START=`date +%s.%N`
 PARTNER_SECRET=`echo "select secret from partner where id=$PARTNER_ID" | mysql -N -h $DB1_HOST -p$DB1_PASS $DB1_NAME -u$DB1_USER`
 PARTNER_ADMIN_SECRET=`echo "select admin_secret from partner where id=$PARTNER_ID" | mysql -N -h $DB1_HOST -p$DB1_PASS $DB1_NAME -u$DB1_USER`
 OUTP=`php $DIRNAME/upload_test.php $SERVICE_URL $PARTNER_ID $PARTNER_SECRET $WEB_DIR/content/templates/entry/data/kaltura_logo_animated_blue.flv 2>&1`
-END=`date +%s.%N`
 RC=$?
+END=`date +%s.%N`
 TOTAL_T=`bc <<< $TIME`
 report "Upload content kaltura_logo_animated_blue.flv" $RC "$OUTP" "`bc <<< $END-$START`"
 
 START=`date +%s.%N`
 OUTP=`php $DIRNAME/upload_test.php $SERVICE_URL $PARTNER_ID $PARTNER_SECRET $WEB_DIR/content/templates/entry/data/kaltura_logo_animated_green.flv 2>&1`
-END=`date +%s.%N`
 RC=$?
+END=`date +%s.%N`
 TOTAL_T=`bc <<< $TIME`
 report "Upload content kaltura_logo_animated_green.flv" $RC "$OUTP" "`bc <<< $END-$START`"
 
 unzip -oqq $WEB_DIR/content/docs/kaltura_batch_upload_falcon.zip -d $WEB_DIR/content/docs
 START=`date +%s.%N`
 OUTP=`php $DIRNAME/upload_bulk.php $SERVICE_URL $PARTNER_ID $PARTNER_ADMIN_SECRET MB $WEB_DIR/content/docs/kaltura_batch_upload_falcon.csv bulkUploadCsv.CSV 2>&1`
-END=`date +%s.%N`
 RC=$?
+END=`date +%s.%N`
 TOTAL_T=`bc <<< $TIME`
 report "Upload bulk using CSV" $RC "$OUTP" "`bc <<< $END-$START`"
 
 START=`date +%s.%N`
 OUTP=`php $DIRNAME/upload_bulk.php $SERVICE_URL $PARTNER_ID $PARTNER_ADMIN_SECRET MB $WEB_DIR/content/docs/kaltura_batch_upload_falcon.xml bulkUploadXml.XML 2>&1`
-END=`date +%s.%N`
 RC=$?
+END=`date +%s.%N`
 TOTAL_T=`bc <<< $TIME`
 report "Upload bulk using XML" $RC "$OUTP" "`bc <<< $END-$START`"
 
 
-START=`date +%s.%N`
 WEBCAM_SYNLINK=`readlink -f $WEB_DIR/content/webcam`
 TEST_NAME="Red5 file upload"
 if [ $WEBCAM_SYNLINK = /usr/lib/red5/webapps/oflaDemo/streams ]; then
+	START=`date +%s.%N`
 	OUTP=`test_red5_conn`
+	RC=$?
+	END=`date +%s.%N`
+	report "$TEST_NAME" $RC "$OUTP" "`bc <<< $END-$START`"
 else
-	echo -e "[${CYAN}$TEST_NAME${NORMAL}] [${BRIGHT_YELLOW}SKIPPED as OflaDemo isn't configured.${NORMAL}]"
+	echo -e "[${CYAN}$TEST_NAME${NORMAL}] ${BRIGHT_YELLOW}[SKIPPED as OflaDemo isn't configured]
+see: https://github.com/kaltura/platform-install-packages/blob/master/doc/install-kaltura-redhat-based.md#configure-red5-server${NORMAL}"
 fi
-END=`date +%s.%N`
+
+START=`date +%s.%N`
+OUTP=`check_missing_web_files`
 RC=$?
-report "$TEST_NAME" $RC "$OUTP" "`bc <<< $END-$START`"
+END=`date +%s.%N`
+if [ $RC -ne 0 ];then
+	report "Check missing web/content files - $OUTP" $RC, $OUTP "`bc <<< $END-$START`"
+else
+	report "Check missing web/content files - none are missing" $RC, "All files found" "`bc <<< $END-$START`"
+
+fi
+
