428c92eb5a7fac0a0fdb5679e2f6b23b7deb5c5e
Jess Portnoy <jess.portnoy@kaltura.com>
2014-11-17 15:30:19 -0500

    kupload now on github.

--- RPM/scripts/build/package_kaltura_kupload.sh
+++ RPM/scripts/build/package_kaltura_kupload.sh
@@ -21,23 +21,25 @@ if [ ! -r $SOURCES_RC ];then
 	exit 1
 fi
 . $SOURCES_RC 
-if [ ! -x `which svn 2>/dev/null` ];then
+if [ ! -x `which wget 2>/dev/null` ];then
+	echo "Need to install svn."
+	exit 2
+fi
+if [ ! -x `which unzip 2>/dev/null` ];then
 	echo "Need to install svn."
 	exit 2
 fi
 
 # remove left overs:
 rm -rf $SOURCE_PACKAGING_DIR/$KUPLOAD_RPM_NAME/*
-
-for KUPLOAD_VERSION in $KUPLOAD_VERSIONS;do
-	svn export --force --quiet $KUPLOAD_URI/$KUPLOAD_VERSION $SOURCE_PACKAGING_DIR/$KUPLOAD_RPM_NAME/$KUPLOAD_VERSION 
-done
-
-cd $SOURCE_PACKAGING_DIR
-
-# flash things DO NOT need exec perms.
-find $KUPLOAD_RPM_NAME -type f -exec chmod -x {} \;
-
-tar jcf $RPM_SOURCES_DIR/$KUPLOAD_RPM_NAME.tar.bz2 $KUPLOAD_RPM_NAME
-echo "Packaged into $RPM_SOURCES_DIR/$KUPLOAD_RPM_NAME.tar.bz2"
+mkdir -p $SOURCE_PACKAGING_DIR/$KUPLOAD_RPM_NAME/
+#for KUPLOAD_VERSION in $KUPLOAD_VERSIONS;do
+#	svn export --force --quiet $KUPLOAD_URI/$KUPLOAD_VERSION $SOURCE_PACKAGING_DIR/$KUPLOAD_RPM_NAME/$KUPLOAD_VERSION 
+#done
+wget $KUPLOAD_URI -O $KUPLOAD_RPM_NAME-$KUPLOAD_VERSION.zip
+unzip $KUPLOAD_RPM_NAME-$KUPLOAD_VERSION.zip
+rm $KUPLOAD_RPM_NAME-$KUPLOAD_VERSION.zip
+cd $SOURCE_PACKAGING_DIR/
+tar zcf  $RPM_SOURCES_DIR/$KUPLOAD_RPM_NAME-$KUPLOAD_VERSION.tar.gz $KUPLOAD_RPM_NAME
+echo "Packaged into $RPM_SOURCES_DIR/$KUPLOAD_RPM_NAME-$KUPLOAD_VERSION.tar.gz"
 rpmbuild -ba $RPM_SPECS_DIR/$KUPLOAD_RPM_NAME.spec
