ad474263bbe3564158b340fa7564512f9d7c1490
Jess Portnoy <jess.portnoy@kaltura.com>
2014-11-18 11:55:39 -0500

    .

--- RPM/scripts/postinst/kaltura-sphinx-schema-update.sh
+++ RPM/scripts/postinst/kaltura-sphinx-schema-update.sh
@@ -28,14 +28,19 @@ if [ ! -r "$RC_FILE" ];then
 	exit 1 
 fi
 . $RC_FILE
-if [ -r $APP_DIR/configurations/sphinx_conf_changed ];then
+# this if would have been very convenient, however, some users may upgrade from say, 2 versions down and not directly from last so we can't really tell their current status and hence, we should do this each upgrade. Since its CE, the running of this should not take that long anyhow.
+#if [ -r $APP_DIR/configurations/sphinx_conf_changed ];then
 	if /etc/init.d/kaltura-sphinx status;then
 		# disable Sphinx's monit monitoring
 		rm $APP_DIR/configurations/monit/monit.d/enabled.sphinx.rc 
 		/etc/init.d/kaltura-sphinx stop
 	fi
-	rm $BASE_DIR/sphinx/kaltura_*  $LOG_DIR/sphinx/data/binlog.*
+	STMP=`date +%s`
+	mkdir -p $BASE_DIR/sphinx.bck.$STMP
+	echo "Backing up files to $BASE_DIR/sphinx.bck.$STMP. Once the upgrade is done and tested, please remove this directory to save space"
+	mv $BASE_DIR/sphinx/kaltura_*  $LOG_DIR/sphinx/data/binlog.* $BASE_DIR/sphinx.bck.$STMP
 	/etc/init.d/kaltura-sphinx start
+	ln -sf $APP_DIR/configurations/monit/monit.avail/sphinx.rc $APP_DIR/configurations/monit/monit.d/enabled.sphinx.rc
 	php $APP_DIR/deployment/base/scripts/populateSphinxEntries.php
 	if [ $? -ne 0 ];then
 
@@ -43,4 +48,4 @@ if [ -r $APP_DIR/configurations/sphinx_conf_changed ];then
 	Please try to run it manually and look at the logs"
 	fi
 	rm $APP_DIR/configurations/sphinx_conf_changed
-fi
+#fi
