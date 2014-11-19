7026e3dd5cdc1dd5fa9d0ad1f368db7ffaeb48b1
Jess Portnoy <jess.portnoy@kaltura.com>
2014-11-19 12:50:34 -0500

    after bouncing ver, also build the meta packages.

--- RPM/scripts/build/bounce_core_ver.sh
+++ RPM/scripts/build/bounce_core_ver.sh
@@ -34,4 +34,8 @@ sed -i "s@\(^Version:\)\s*.*\$@\1 $NEWVER@g" kaltura-base.spec kaltura-batch.spe
 sed -i "s@\(^Release:\)\s*.*\$@\1 1@g" kaltura-base.spec kaltura-batch.spec kaltura-front.spec kaltura-release.spec kaltura-server.spec
 sed -i "s^\(%changelog\)^\1\n* $STAMP $PACKAGER_NAME <$PACKAGER_MAIL> - $NEWVER-1\n- Ver Bounce to $NEWVER\n^" kaltura-base.spec kaltura-batch.spec kaltura-front.spec kaltura-release.spec kaltura-server.spec
 
+rpmbuild -ba kaltura-batch.spec
+rpmbuild -ba kaltura-front.spec
+rpmbuild -ba kaltura-release.spec
+rpmbuild -ba kaltura-server.spec
 
