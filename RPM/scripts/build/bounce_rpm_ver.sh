4ff4bd1e9707400a118ef78a13df457bf44bc1af
Jess Portnoy <jess.portnoy@kaltura.com>
2014-12-01 12:40:23 -0500

    .

---
+++ RPM/scripts/build/bounce_rpm_ver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash - 
+#===============================================================================
+#          FILE: bounce_ver.sh
+#         USAGE: ./bounce_ver.sh 
+#   DESCRIPTION: 
+#       OPTIONS: ---
+# 	LICENSE: AGPLv3+
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jess Portnoy , <jess.portnoy@kaltura.com>
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED: 03/09/14 06:59:26 EDT
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+if [ $# -lt 1 ];then
+	echo "Usage $0 <path/to/spec> <new ver>"
+	exit 1
+fi
+SPEC_FILE=$1
+NEWVER=$2
+SOURCES_RC=`dirname $0`/sources.rc
+if [ ! -r $SOURCES_RC ];then
+	echo "Could not find $SOURCES_RC"
+	exit 1
+fi
+. $SOURCES_RC 
+STAMP=`date "+%-a %b %-d %Y"`
+cd $RPM_SPECS_DIR
+
+sed -i "s@\(^Version:\)\s*.*\$@\1 $NEWVER@g" $SPEC_FILE
+sed -i "s@\(^Release:\)\s*.*\$@\1 1@g" $SPEC_FILE
+sed -i "s^\(%changelog\)^\1\n* $STAMP $PACKAGER_NAME <$PACKAGER_MAIL> - $NEWVER-1\n- Ver Bounce to $NEWVER\n^" $SPEC_FILE
+
+
