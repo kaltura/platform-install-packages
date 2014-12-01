e964f7d388af1082f447a8991aaf28e595377c6b
Jess Portnoy <jess.portnoy@kaltura.com>
2014-12-01 12:25:01 -0500

    since nightly will need to input changelog entries as well and not only to Core packages, added bounce_rpm_ver.sh which the bounce_core_ver.sh wrapper now calls.

--- RPM/scripts/build/bounce_rpm_ver.sh
+++ RPM/scripts/build/bounce_rpm_ver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash - 
 #===============================================================================
-#          FILE: bounce_core_ver.sh
-#         USAGE: ./bounce_core_ver.sh 
+#          FILE: bounce_ver.sh
+#         USAGE: ./bounce_ver.sh 
 #   DESCRIPTION: 
 #       OPTIONS: ---
 # 	LICENSE: AGPLv3+
@@ -17,10 +17,11 @@
 #set -o nounset                              # Treat unset variables as an error
 
 if [ $# -lt 1 ];then
-	echo "Usage $0 <new core ver>"
+	echo "Usage $0 <path/to/spec> <new ver>"
 	exit 1
 fi
-NEWVER=$1
+SPEC_FILE=$1
+NEWVER=$2
 SOURCES_RC=`dirname $0`/sources.rc
 if [ ! -r $SOURCES_RC ];then
 	echo "Could not find $SOURCES_RC"
@@ -28,15 +29,10 @@ if [ ! -r $SOURCES_RC ];then
 fi
 . $SOURCES_RC 
 STAMP=`date "+%-a %b %-d %Y"`
-BASE_DIR=`dirname $0`
+cd $RPM_SPECS_DIR
 
+sed -i "s@\(^Version:\)\s*.*\$@\1 $NEWVER@g" $SPEC_FILE
+sed -i "s@\(^Release:\)\s*.*\$@\1 1@g" $SPEC_FILE
+sed -i "s^\(%changelog\)^\1\n* $STAMP $PACKAGER_NAME <$PACKAGER_MAIL> - $NEWVER-1\n- Ver Bounce to $NEWVER\n^" $SPEC_FILE
 
-for i in $RPM_SPECS_DIR/kaltura-batch.spec $RPM_SPECS_DIR/kaltura-front.spec $RPM_SPECS_DIR/kaltura-release.spec $RPM_SPECS_DIR/kaltura-server.spec;do
-	$BASE_DIR/bounce_rpm_ver.sh $i $NEWVER
-	#rpmbuild -ba $i
-done
-
-# we run this one of the loop because we don't want to build it yet since we need to manually add the changelog to it.
-# the rest can do with the default "bounce to $VER" message since they'e just meta packages.
-`dirname $0`/bounce_rpm_ver.sh $RPM_SPECS_DIR/kaltura-base.spec $NEWVER
 
