dc8a9e7f60ee797d92a181bc832166fcdab19bb2
Jess Portnoy <jess.portnoy@kaltura.com>
2014-12-14 13:41:44 -0500

    script to package kaltura-postinst.

---
+++ RPM/scripts/build/package_kaltura_postinst.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e 
+#===============================================================================
+#          FILE: package_kaltura_core.sh
+#         USAGE: ./package_kaltura_core.sh 
+#   DESCRIPTION: 
+#       OPTIONS: ---
+# 	LICENSE: AGPLv3+
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jess Portnoy (), <jess.portnoy@kaltura.com>
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED: 01/10/14 08:46:43 EST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+SOURCES_RC=`dirname $0`/sources.rc
+if [ ! -r $SOURCES_RC ];then
+	echo "Could not find $SOURCES_RC"
+	exit 1
+fi
+. $SOURCES_RC 
+cd $SOURCE_PACKAGING_DIR/platform-install-packages/RPM/scripts/ 
+tar zcvf ~/rpmbuild/SOURCES/kaltura-postinst-$KALTURA_POSTINST_VERSION.tar.gz postinst/
+echo "Packaged into $RPM_SOURCES_DIR/kaltura-postinst-$KALTURA_POSTINST_VERSION.tar.gz"
+rpmbuild -ba $RPM_SPECS_DIR/kaltura-postinst.spec
