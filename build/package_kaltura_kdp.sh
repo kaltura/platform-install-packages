3df4127b49425aa390f6e6deb9a91c03d34ffae5
Jess Portnoy <jess.portnoy@kaltura.com>
2015-01-05 15:33:12 +0000

    there is only one KDP_VERSION packaged, not several, unlike KDP3 where there ARE a few.

--- build/package_kaltura_kdp.sh
+++ build/package_kaltura_kdp.sh
@@ -26,14 +26,12 @@ if [ ! -x "`which svn 2>/dev/null`" ];then
 	exit 2
 fi
 
-for KDP_VERSION in $KDP_VERSIONS;do
-	kaltura_svn export --force --quiet $KDP_URI/$KDP_VERSION $SOURCE_PACKAGING_DIR/$KDP_RPM_NAME/$KDP_VERSION 
-done
+kaltura_svn export --force --quiet $KDP_URI/$KDP_VERSION $SOURCE_PACKAGING_DIR/$KDP_RPM_NAME/$KDP_VERSION 
 cd $SOURCE_PACKAGING_DIR
 # flash things DO NOT need exec perms.
 find $KDP_RPM_NAME -type f -exec chmod -x {} \;
-tar jcf $RPM_SOURCES_DIR/$KDP_RPM_NAME.tar.bz2 $KDP_RPM_NAME
-echo "Packaged into $RPM_SOURCES_DIR/$KDP_RPM_NAME.tar.bz2"
+tar jcf $RPM_SOURCES_DIR/$KDP_RPM_NAME-$KDP_VERSION.tar.bz2 $KDP_RPM_NAME
+echo "Packaged into $RPM_SOURCES_DIR/$KDP_RPM_NAME-$KDP_VERSION.tar.bz2"
 if [ -x "`which rpmbuild 2>/dev/null`" ];then
 	rpmbuild -ba $RPM_SPECS_DIR/$KDP_RPM_NAME.spec
 fi
