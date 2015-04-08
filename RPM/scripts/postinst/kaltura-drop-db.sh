e9b3a118d52ebbf1736bee58c8da20afd9105759
Jess Portnoy <jess.portnoy@kaltura.com>
2015-04-08 16:11:20 +0000

    - kaltura-drop-db.sh can now run from deb postinst as well as from shell interactively - added dialogs for VOD packager and DB questions

--- RPM/scripts/postinst/kaltura-drop-db.sh
+++ RPM/scripts/postinst/kaltura-drop-db.sh
@@ -31,22 +31,26 @@ echo -en "This will drop the following DBs:
 $DBS 
 and remove users:
 $DB_USERS
-on $DB1_HOST
-${BRIGHT_RED}
+on $DB1_HOST"
+if [ -n "$1" ];then
+	DBPASSWD=$1
+else
+	echo -en "${BRIGHT_RED}
 NOTE: this is not reversible. 
 It is recommended you also back up the current data using mysqldump before continuing.
 You can use /opt/kaltura/bin/kaltura-export-db.sh to export the data.
 
 Are you absolutely certain you want this? [n/Y]
 ${NORMAL}
-"
-read AN
-if [ "$AN" != 'Y' ];then
-	echo "Aborting. To remove hit UPPER CASED 'Y'"
-	exit 1
+	"
+	read AN
+	if [ "$AN" != 'Y' ];then
+		echo "Aborting. To remove hit UPPER CASED 'Y'"
+		exit 1
+	fi
+	echo "root DB passwd:"
+	read -s DBPASSWD
 fi
-echo "root DB passwd:"
-read -s DBPASSWD
 for i in $DBS;do
 	echo "Removing $i" 
 	echo "drop database $i" | mysql -h$DB1_HOST -p$DBPASSWD -P$DB1_PORT ;
