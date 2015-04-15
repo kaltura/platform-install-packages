7dd00ce6e7b9e3da3564b278560daf6c13106f2c
Jess Portnoy <jess.portnoy@kaltura.com>
2015-04-15 17:57:07 +0000

    enable work with a remote db and typoe correction.

--- RPM/scripts/postinst/drop-dwh-data.sh
+++ RPM/scripts/postinst/drop-dwh-data.sh
@@ -23,15 +23,15 @@ fi
 echo "This will remove all data from the DWH [analytics] DBs and cannot be reverted.
 Are you sure you want to continue? [N/y]"
 read ANS
-if [ "$ANS" !='y' ];then
+if [ "$ANS" != 'y' ];then
 	echo "Exiting. Rerun and select lower case 'y' to continue"
 	exit 2
 fi
 PASSWD=$1
-for i in `mysql -N -p$PASSWD kalturadw_bisources -e "show tables"`;do mysql -p$PASSWD kalturadw_bisources -e "drop table $i";done
-for i in `mysql -N -p$PASSWD kalturadw -e "show tables"`;do mysql -p$PASSWD kalturadw -e "drop table $i";done
-for i in `mysql -N -p$PASSWD kalturadw_ds -e "show tables"`;do mysql -p$PASSWD kalturadw_ds -e "drop table $i";done
-for i in `mysql -N -p$PASSWD kalturalog -e "show tables"`;do mysql -p$PASSWD kalturalog -e "drop table $i";done
-for i in `mysql -p$PASSWD -e "Show procedure status" |grep kalturadw|awk -F " " '{print $2}'`;do mysql kalturadw -p$PASSWD -e "drop procedure $i;";done
-for i in `mysql -p$PASSWD -e "Show procedure status" |grep kalturadw_ds|awk -F " " '{print $2}'`;do mysql kalturadw_ds -p$PASSWD -e "drop procedure $i;";done
+for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturadw_bisources -e "show tables"`;do mysql -p$PASSWD kalturadw_bisources -e "drop table $i";done
+for i in `mysql -h $DB1_HOST  -N -p$PASSWD kalturadw -e "show tables"`;do mysql -p$PASSWD kalturadw -e "drop table $i";done
+for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturadw_ds -e "show tables"`;do mysql -p$PASSWD kalturadw_ds -e "drop table $i";done
+for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturalog -e "show tables"`;do mysql -p$PASSWD kalturalog -e "drop table $i";done
+for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show procedure status" |grep kalturadw|awk -F " " '{print $2}'`;do mysql kalturadw -p$PASSWD -e "drop procedure $i;";done
+for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show procedure status" |grep kalturadw_ds|awk -F " " '{print $2}'`;do mysql kalturadw_ds -p$PASSWD -e "drop procedure $i;";done
 
