aa203e75ff7522106c51990ba684594d71eefde9
Jess Portnoy <jess.portnoy@kaltura.com>
2015-07-10 17:09:40 +0000

    script now auto detects distro and codename and adds repos as needed.

--- deb/install_kaltura_all_in_1.sh
+++ deb/install_kaltura_all_in_1.sh
@@ -8,14 +8,31 @@ echo "deb [arch=amd64] http://installrepo.kaltura.org/repo/apt/debian jupiter ma
 if which aptitude;then
         APT_TOOL="aptitude -y"
 else
-        APT_TOOL=$APT_TOOL
+        APT_TOOL="apt-get -y"
 fi
-$APT_TOOL -y update
+$APT_TOOL update
+$APT_TOOL install lsb-release 
+# for Debian, the non-free repo must also be enabled
+# Ubuntu: You must also make sure the multiverse repo is enabled in /etc/apt/sources.list
+# Debian Jessie [8]: You must also make sure the following Wheezy repos are enabled in /etc/apt/sources.list
+DISTRO=`lsb_release -s -i`
+if [ "$DISTRO" = 'Ubuntu' ];then
+	apt-add-repository multiverse
+elif [ "$DISTRO" = 'Debian' ];then
+	$APT_TOOL install software-properties-common
+	apt-add-repository non-free
+	CODENAME=`lsb_release  -s -c`
+	if [ "$CODENAME" != 'wheezy' ];then
+		apt-add-repository "deb http://ftp.debian.org/debian/ wheezy main"
+		apt-add-repository "deb http://security.debian.org/ wheezy/updates main"
+	fi
+fi
+exit;
 $APT_TOOL install mysql-server
 $APT_TOOL install kaltura-postinst
 $APT_TOOL install kaltura-base
+$APT_TOOL install kaltura-widgets kaltura-html5lib kaltura-html5-studio
 $APT_TOOL install kaltura-front
-$APT_TOOL install kaltura-widgets kaltura-html5-studio kaltura-html5lib
 $APT_TOOL install kaltura-sphinx
 $APT_TOOL install kaltura-db
 $APT_TOOL install kaltura-batch
