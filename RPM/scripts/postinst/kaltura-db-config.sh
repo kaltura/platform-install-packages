01cb095c2b09f76f56297bcc5b27e1f457b88f13
kobimichaeli <kobi.michaeli@kaltura.com>
2015-09-30 12:01:46 +0300

    Fixed a typo in the if condition
    (missing ")

--- RPM/scripts/postinst/kaltura-db-config.sh
+++ RPM/scripts/postinst/kaltura-db-config.sh
@@ -204,7 +204,7 @@ rm -f $APP_DIR/log/kaltura-*.log
 
 
 if [ "$DB1_HOST" = `hostname` -o "$DB1_HOST" = '127.0.0.1' -o "$DB1_HOST" = 'localhost' ];then
-	if [ `rpm -qa "Percona-Server-server*` ]; then 
+	if [ `rpm -qa "Percona-Server-server*"` ]; then 
 		ln -sf $BASE_DIR/app/configurations/monit/monit.avail/percona.rc $BASE_DIR/app/configurations/monit/monit.d/enabled.mysqld.rc
 	else
 		ln -sf $BASE_DIR/app/configurations/monit/monit.avail/mysqld.rc $BASE_DIR/app/configurations/monit/monit.d/enabled.mysqld.rc
