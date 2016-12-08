6adec83f06f0ba6cb3d9541daf2cb02146c8ee4a
jpluijmers <joost@vixyvideo.com>
2016-12-08 17:21:05 +0100

    Includes system.ini, user -host on executing queries and clears functions/views/procedures more thoroughly.

--- RPM/scripts/postinst/drop-dwh-data.sh
+++ RPM/scripts/postinst/drop-dwh-data.sh
@@ -15,6 +15,8 @@
 
 #set -o nounset                              # Treat unset variables as an error
 
+. /etc/kaltura.d/system.ini
+
 if [ $# -lt 1 ];then
 	echo "Usage: $0 <mysql_root_passwd>"
 	exit 1
@@ -28,10 +30,14 @@ if [ "$ANS" != 'y' ];then
 	exit 2
 fi
 PASSWD=$1
-for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturadw_bisources -e "show tables"`;do mysql -p$PASSWD kalturadw_bisources -e "drop table $i";done
-for i in `mysql -h $DB1_HOST  -N -p$PASSWD kalturadw -e "show tables"`;do mysql -p$PASSWD kalturadw -e "drop table $i";done
-for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturadw_ds -e "show tables"`;do mysql -p$PASSWD kalturadw_ds -e "drop table $i";done
-for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturalog -e "show tables"`;do mysql -p$PASSWD kalturalog -e "drop table $i";done
-for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show procedure status" |grep kalturadw|awk -F " " '{print $2}'`;do mysql kalturadw -p$PASSWD -e "drop procedure $i;";done
-for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show procedure status" |grep kalturadw_ds|awk -F " " '{print $2}'`;do mysql kalturadw_ds -p$PASSWD -e "drop procedure $i;";done
+for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturadw_bisources -e "show tables"`;do mysql -h $DB1_HOST -p$PASSWD kalturadw_bisources -e "drop table $i";done
+for i in `mysql -h $DB1_HOST  -N -p$PASSWD kalturadw -e "select TABLE_NAME from information_schema.views where TABLE_SCHEMA = 'kalturadw';"`;do mysql -h $DB1_HOST -p$PASSWD kalturadw -e "drop view $i";done
+for i in `mysql -h $DB1_HOST  -N -p$PASSWD kalturadw -e "show tables"`;do mysql -h $DB1_HOST -p$PASSWD kalturadw -e "drop table $i";done
+for i in `mysql -h $DB1_HOST  -N -p$PASSWD kalturadw -e "select TABLE_NAME from information_schema.views where TABLE_SCHEMA = 'kalturadw_ds';"`;do mysql -h $DB1_HOST -p$PASSWD kalturadw_ds -e "drop view $i";done
+for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturadw_ds -e "show tables"`;do mysql -h $DB1_HOST -p$PASSWD kalturadw_ds -e "drop table $i";done
+for i in `mysql -h $DB1_HOST -N -p$PASSWD kalturalog -e "show tables"`;do mysql -h $DB1_HOST -p$PASSWD kalturalog -e "drop table $i";done
+for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show function status" |grep kalturadw|awk -F " " '{print $2}'`;do mysql kalturadw -h $DB1_HOST -p$PASSWD -e "drop function $i;";done
+for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show procedure status" |grep kalturadw|awk -F " " '{print $2}'`;do mysql kalturadw -h $DB1_HOST -p$PASSWD -e "drop procedure $i;";done
+for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show function status" |grep kalturadw_ds|awk -F " " '{print $2}'`;do mysql kalturadw_ds -h $DB1_HOST -p$PASSWD -e "drop function $i;";done
+for i in `mysql -h $DB1_HOST -p$PASSWD -e "Show procedure status" |grep kalturadw_ds|awk -F " " '{print $2}'`;do mysql kalturadw_ds -h $DB1_HOST -p$PASSWD -e "drop procedure $i;";done
 
